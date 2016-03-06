f49a6251ab42e32c97ebcff47fb3ca1914911f56
amanmehara <amanmehara@hotmail.com>
2016-03-06 16:44:01 +0530

    Added training_dataset
    Functionality for reading samples from a file created.
    Attributes successfully read & converted into integer.

--- src/Learning.java
+++ src/Learning.java
@@ -1,5 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Learning {
     public static void main(String Args[]) {
+        BufferedReader bufferedReader;
 
+        List<int[]> samples = new ArrayList<>();
+        String sample;
+
+        try {
+            bufferedReader=new BufferedReader(new FileReader("training_dataset"));
+            while ((sample=bufferedReader.readLine())!=null) {
+
+                samples.add(Arrays
+                        .stream(sample.split(","))
+                        .map(String::trim)
+                        .mapToInt(Integer::parseInt)
+                        .toArray());
+
+                //samples.add(sample.split(","));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int[] args: samples) {
+            int sum=0;
+            for (int arg: args) {
+                sum+=arg;
+            }
+            System.out.println(sum);
+        }
     }
 }
