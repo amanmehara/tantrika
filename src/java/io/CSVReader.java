42ee7c12321bd32afccf4ffaf00a142ad77b117e
Aman Mehara <amanmehara@hotmail.com>
2019-05-21 21:51:50 +0530

    IO... :pencil:
    CSVReader.

---
+++ src/java/io/CSVReader.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 Aman Mehara
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io;
+
+import math.linalg.Matrix;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class CSVReader {
+
+    private final Path path;
+
+    public CSVReader(Path path) {
+        this.path = path;
+    }
+
+    public Matrix read() throws IOException {
+        Function<String, Double[]> parseLine = line -> Arrays
+                .stream(line.split(","))
+                .map(String::trim)
+                .map(Double::parseDouble)
+                .toArray(Double[]::new);
+
+        try (Stream<String> lines = Files.lines(path)) {
+            return new Matrix(lines.map(parseLine).toArray(Double[][]::new));
+        }
+    }
+
+}
